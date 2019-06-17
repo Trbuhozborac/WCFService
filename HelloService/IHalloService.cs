89e6c2e957cffebdbbbfd46dd662fbf5923713c9
pomorange <nemanjapipi@hotmail.com>
2019-06-17 23:05:31 +0200

    kreirao host

--- HelloService/IHalloService.cs
+++ HelloService/IHalloService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace HelloService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHalloService" in both code and config file together.
-    [ServiceContract]
+    
     public interface IHalloService
     {
         [OperationContract]
---
+++ HelloServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HalloService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
